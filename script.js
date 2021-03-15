bde58c5aa2a78c954995c51d29f5fbdd2bb6e933
ZSolt <zsoltika192@gmail.com>
2021-03-15 13:53:47 +0100

    Feat: Pregmacth

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 function evKalk(){
   var born = document.getElementById('born').value;
 
-  born = born.split("-");
-  var year = parseInt(born[0]);
-  var month = parseInt(born[1]);
-  var day = parseInt(born[2]);
-
-  var age = new Date().getFullYear() - year;
-
-  var zodiacList = [
-    "Bak",
-    "Vízöntő",
-    "Halak",
-    "Kos",
-    "Bika",
-    "Ikrek",
-    "Rák",
-    "Mérleg",
-    "Szűz",
-    "Skorpió",
-    "Nyilas",
-  ];
-
-  if(day < 20) month--;
-  month = month %12;
-
-  var zodiac = zodiacList[month];
-
-  var response = `Te most ${age} éves lehetsz, a csillagjegyed pedig ${zodiac}`;
-
-
-  alert(response);
+  if(born.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/))
+  {
+    born = born.split("-");
+    var year = parseInt(born[0]);
+    var month = parseInt(born[1]);
+    var day = parseInt(born[2]);
+  
+    var age = new Date().getFullYear() - year;
+  
+    var zodiacList = [
+      "Bak",
+      "Vízöntő",
+      "Halak",
+      "Kos",
+      "Bika",
+      "Ikrek",
+      "Rák",
+      "Mérleg",
+      "Szűz",
+      "Skorpió",
+      "Nyilas",
+    ];
+  
+    if(day < 20) month--;
+    month = month %12;
+  
+    var zodiac = zodiacList[month];
+  
+    var response = `Te most ${age} éves lehetsz, a csillagjegyed pedig ${zodiac}`;
+  
+  
+    alert(response);
+  }
+  else alert("Nem megfelelő formátum!");
 };
+
+
 
 
 
