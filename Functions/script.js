d991c05a1f05f587b8c3b085608d09a74c0ee603
ZSolt <zsoltika192@gmail.com>
2021-03-15 18:21:51 +0100

    Unnamed function

---
+++ Functions/script.js
@@ -0,0 +1,21 @@
+//Névtelen függvény
+var numbers = [12,321,115,54,8,93,89,76,67];
+
+numbers.sort(function(a,b){
+    return a % 10 - b % 10;
+
+})
+
+
+
+
+
+
+
+
+//Önhívó függyvény
+
+
+
+
+//Person "osztály" (ES6 előtt)
