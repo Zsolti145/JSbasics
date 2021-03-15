b85d90d07f525f9a43eb4808e2a439af007658b4
ZSolt <zsoltika192@gmail.com>
2021-03-15 18:31:19 +0100

    Recursive Function

--- Functions/script.js
+++ Functions/script.js
@@ -4,16 +4,31 @@ var numbers = [12,321,115,54,8,93,89,76,67];
 numbers.sort(function(a,b){
     return a % 10 - b % 10;
 
+    console.log(numbers);
 })
 
 
-
-
-
-
-
-
 //Önhívó függyvény
+var main = (function()
+{
+console.log("Fut a függvény");
+
+var step =1;
+var methods = [
+    function(){step++;},
+    function(){return step;}
+];
+return methods;
+
+})();
+
+var GetStep = main[1];
+var IncStep = main[0];
+
+IncStep();
+IncStep();
+IncStep();
+console.log(GetStep());
 
 
 
